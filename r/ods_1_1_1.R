86b44a865458a30ed4cf9694804b26f67b633856
evandro@stk9 <evandroluisalves13@gmail.com>
2024-06-06 17:59:57 -0300

    Acrescenta linhas de totais e reestrutura fluxo para pnad_raw -> ods_clean -> ods (estimativa)

---
+++ r/ods_1_1_1.R
@@ -0,0 +1,88 @@
+
+# 1. Packages ---------------------------------------------------------------------
+pacotes = c("dplyr",
+            "purrr",
+            "tibble",
+            "stringr",
+            "tidyr",
+            "readr",
+            "xlsx",
+            "here",
+            "survey",
+            "tictoc")
+
+#
+lapply(pacotes, library, character.only = T)
+
+# 2. Leitura de bases -------------------------------------------------------------
+
+list_db <- read_rds(
+  here("data", "2_clean", "ods_pnad_clean.rds")
+)
+
+# 3. Parâmetros  ---------------------------------------------------------------------
+
+subgroups <- c(
+  "ano","sexo","raca", "local"
+)
+
+list_subgroups <- list(
+  c("ano"),
+  c("ano","sexo"),
+  c("ano","sexo","raca"),
+  c("ano","sexo","raca", "local")
+)
+
+#
+expr <- paste0("interaction(", paste(subgroups, collapse = ", "), ")")
+list_expr <- map(list_subgroups, ~paste0("interaction(", paste(.x, collapse = ", "), ")"))
+
+
+# 3. Função ------------------------------------------------------------------
+
+#
+get_ind_1_1_1 <- function(db) {
+  map2(
+    list_subgroups,
+    list_expr,
+    ~svyby(
+      formula = ~n_abaixo_ln_pobreza_ext,
+      by = as.formula(paste0("~", .y)),
+      design = db,
+      FUN = svymean,
+      vartype = "ci",
+      na.rm = TRUE) %>%
+      as.data.frame() %>%
+      separate_wider_delim(.y, delim = ".", names = .x)
+  )
+}
+
+
+# 4. Estimativa -----------------------------------------------------------
+
+
+# 236.042 sec elapsed [~4 minutos]
+# Até 15GiB
+tic()
+ind_1_1_1 <- map_df(
+  list_db,
+  ~get_ind_1_1_1(db = .x)
+)
+toc()
+
+# 5. Exporta -------------------------------------------------------------------
+
+# Cria lista com objetos do environment 
+list_outputs <- mget(ls(.GlobalEnv))
+
+# Cria pastas nomeadas de acordo com os nomes dos objetos
+names(list_outputs) %>%
+  map(~fs::dir_create(here("outputs", "mvp", .x)))
+
+# Cria arquivos em formato planilha/.xlsx
+mget(ls(list_outputs, pattern = "^ind_.*")) %>%
+  map2(names(.),
+       ~writexl::write_xlsx(.x,
+                            here("outputs", "mvp", .y,
+                                 paste0(.y, ".xlsx")))
+  )
---
+++ r/ods_pnad_clean.R
@@ -0,0 +1,136 @@
+
+# 1. Packages ---------------------------------------------------------------------
+pacotes = c("dplyr",
+            "purrr",
+            "tibble",
+            "stringr",
+            "tidyr",
+            "readr",
+            "xlsx",
+            "here",
+            "survey",
+            "tictoc")
+
+#
+lapply(pacotes, library, character.only = T)
+
+# 2. Leitura de bases -------------------------------------------------------------
+
+list_db_raw <- list(
+  read_rds(here("data", "1_raw", "pnad", "pnad_1_2015.rds")),
+  read_rds(here("data", "1_raw", "pnad", "pnad_1_2022.rds"))
+)
+
+# 3. Parâmetros -----------------------------------------------------------
+
+# PPC R$ 2,33 para US$ 1,00 (2017)
+# duvida: atualizar para o valor de 2022 (2,56)? https://data.worldbank.org/indicator/PA.NUS.PRVT.PP
+ppc = 2.3273771
+ln_pobreza_ext = ppc*3.2
+ln_pobreza = ppc*6.85
+
+# 4. Ajutes de base --------------------------------------------------------------- 
+
+# sexo V2007
+# cor/raca V2010
+# faixa etaria VD2006
+# status de ocupação (IPEA coloca apenas calculo de todos os outros pra pessoas ocupadas somente)
+# localização geográfica (urbano/rural) V1022
+# território étnico-racial (IPEA usa regiao)
+
+# VD5002- Rendimento (efetivo) domiciliar per capita 
+# (exclusive rendimentos em cartão/tíquete transporte ou alimentação) 
+# (exclusive o rendimento das pessoas cuja condição na unidade domiciliar era pensionista, 
+# empregado doméstico ou parente do empregado doméstico) 
+
+
+list_db <- map(
+  list_db_raw,
+  ~update(
+    .x,
+    ano = Ano,
+    sexo = case_when(
+      V2007 %in% c("Homem", "Masculino") ~ "Masculino",
+      V2007 %in% c("Mulher", "Masculino") ~ "Feminino"
+    ),
+    local = V1022,
+    VD5002_defl = VD5002*CO2e,
+    VD5002_defl_dia = VD5002_defl/30,
+    renda_hora_defl = VD5002_defl_dia/24,
+    # dummy_ln_pobreza_ext = as.factor(ifelse(VD5002_defl_dia >= ln_pobreza_ext, 0, 1)),
+    # dummy_ln_pobreza = as.factor(ifelse(VD5002_defl_dia >= ln_pobreza, 0, 1)),
+    n_abaixo_ln_pobreza_ext = case_when(
+      VD5002_defl_dia < ln_pobreza_ext ~ 1,
+      VD5002_defl_dia >= ln_pobreza_ext ~ 0,
+      TRUE ~ NA_real_),
+    # n_acima_ln_pobreza_ext = case_when(
+    #   VD5002_defl_dia >= ln_pobreza_ext ~ 1,
+    #   VD5002_defl_dia < ln_pobreza_ext ~ 0,
+    #   TRUE ~ NA_real_),
+    n_abaixo_ln_pobreza = case_when(
+      VD5002_defl_dia < ln_pobreza ~ 1,
+      VD5002_defl_dia >= ln_pobreza ~ 0,
+      TRUE ~ NA_real_),
+    n_desocupados = case_when(
+      VD4002 == "Pessoas desocupadas" ~ 1,
+      VD4002 == "Pessoas ocupadas" ~ 0,
+      TRUE ~ NA_real_),
+    n_ocupados = case_when(
+      VD4002 == "Pessoas desocupadas" ~ 0,
+      VD4002 == "Pessoas ocupadas" ~ 1,
+      TRUE ~ NA_real_),
+    raca = case_when(
+      V2010 %in% c("Preta", "Parda") ~ "Preto ou pardo",
+      TRUE ~ V2010), 
+    regiao = as.factor(case_when(
+      UF %in% c("Rondônia", "Acre", "Amazonas", "Roraima", "Pará", "Amapá", "Tocantins") ~ "Norte",
+      UF %in% c("Maranhão", "Piauí", "Ceará", "Rio Grande do Norte", "Paraíba", 
+                "Pernambuco", "Alagoas", "Sergipe", "Bahia") ~ "Nordeste",
+      UF %in% c("Mato Grosso do Sul", "Mato Grosso", "Goiás", "Distrito Federal") ~ "Centro-Oeste",
+      UF %in% c("Minas Gerais", "Espírito Santo", "Rio de Janeiro", "São Paulo") ~ "Sudeste",
+      UF %in% c("Paraná", "Rio Grande do Sul", "Santa Catarina") ~ "Sul",
+      TRUE ~ NA_character_
+    )),
+    n_ef_comp = case_when(
+      VD3004 %in% c(
+        "Fundamental completo ou equivalente",
+        "Médio incompleto ou equivalente",
+        "Médio completo ou equivalente",          
+        "Superior incompleto ou equivalente",
+        "Superior completo"
+      )~ 1,
+      VD3004 %in% c(
+        "Sem instrução e menos de 1 ano de estudo",
+        "Fundamental incompleto ou equivalente"
+      ) ~ 0,
+      TRUE ~ NA_real_
+    ),
+    n_em_comp = case_when(
+      VD3004 %in% c(
+        "Médio completo ou equivalente",          
+        "Superior incompleto ou equivalente",
+        "Superior completo"
+      ) ~ 1,
+      VD3004 %in% c(
+        "Sem instrução e menos de 1 ano de estudo",
+        "Fundamental incompleto ou equivalente",
+        "Fundamental completo ou equivalente",
+        "Médio incompleto ou equivalente"
+      ) ~ 0,
+      TRUE ~ NA_real_
+    )
+  )
+)
+
+# 5. Exporta --------------------------------------------------------------
+
+#
+fs::dir_create(here("data", "2_clean"))
+
+#
+saveRDS(
+  list_db,
+  here("data", "2_clean", "ods_pnad_clean.rds")
+)
+
+
